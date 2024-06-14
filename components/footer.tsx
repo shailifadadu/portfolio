4ee6ad1f071d5525a05291a45b1791ae9352d472
shailifadadu <116940934+shailifadadu@users.noreply.github.com>
2024-06-14 22:26:46 +0530

    Updated footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,36 @@
 import React from "react";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
-      <small className="mb-2 block text-xs">
-        &copy; 2024 Shaily. All rights reserved.
-      </small>
-      <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        React & Next.js, TypeScript, Tailwind CSS, Framer Motion, React Email &
-        Resend, Vercel hosting.
-      </p>
+      <span className="font-semibold">Made With 💖 by Shaily Fadadu</span>
+      <div className="mt-4">
+        <a
+          href="https://github.com/shailifadadu"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block mx-2 text-gray-500 hover:text-gray-700"
+        >
+          <FaGithub className="w-5 h-5" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/shailyfadadu/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block mx-2 text-gray-500 hover:text-gray-700"
+        >
+          <FaLinkedin className="w-5 h-5" />
+        </a>
+        <a
+          href="https://x.com/ShailyFadadu"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block mx-2 text-gray-500 hover:text-gray-700"
+        >
+          <FaTwitter className="w-5 h-5" />
+        </a>
+      </div>
     </footer>
   );
 }
