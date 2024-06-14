b62b30c6299f46556914e1e75c11b36ad8d356cd
shailifadadu <116940934+shailifadadu@users.noreply.github.com>
2024-06-14 11:55:32 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import Header from "@/components/header";
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Shaily Fadadu",
+  description: "It is personal portfolio of Shaily",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}
+      >
+        <div
+          className="bg-[#fbe2e3] relative top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]
+        sm:w-[68.75rem]"
+        ></div>
+        <div
+          className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem]
+        sm:w-[68.75rem] md:left-[-33rem] lg:left-[-8rem] xl:left-[-15rem] 2xl:left-[-5rem]"
+        ></div>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
