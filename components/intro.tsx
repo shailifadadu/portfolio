15e94cf62ab5dac8ca84f849fdb7ca847265f13c
shailifadadu <116940934+shailifadadu@users.noreply.github.com>
2024-06-14 17:08:47 +0530

    Added Email functionality

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,8 +31,8 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Ricardo portrait"
+              src="./profile.jpg"
+              alt="Shaily Fadadu"
               width="192"
               height="192"
               quality="95"
