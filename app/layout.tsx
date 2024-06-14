f01d300cf172a79275fc0e27eb90fb0a32b0ef2b
shailifadadu <116940934+shailifadadu@users.noreply.github.com>
2024-06-14 23:13:53 +0530

    Changed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Shaily Fadadu",
+  title: "Shaily Fadadu | Portfolio",
   description: "Its my portfolio.",
 };
 
