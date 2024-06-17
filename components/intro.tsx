380a4a59609bf65296bba711b2411dc855daa124
shailifadadu <116940934+shailifadadu@users.noreply.github.com>
2024-06-17 11:22:25 +0530

    Updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -41,7 +41,7 @@ export default function Intro() {
             />
           </motion.div>
 
-          <motion.span
+          {/*  <motion.span
             className="absolute bottom-0 right-0 text-4xl"
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
@@ -53,7 +53,7 @@ export default function Intro() {
             }}
           >
             👋
-          </motion.span>
+          </motion.span> */}
         </div>
       </div>
 
